2c4e58c0d530a0afa32472c7ce4d8edd0cc80495
mdeumie <2m1tema@gmail.com>
2015-12-09 15:30:45 +0100

    ajout img menu / design material / route angular

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -9,6 +9,30 @@ angular.module('myApp.view2', ['ngRoute'])
   });
 }])
 
-.controller('View2Ctrl', [function() {
+.controller('View2Ctrl', ['$scope','$http', function($scope, $http) {
+    $scope.users = [
+        {
+            name: '',
+            mail: ''
+        }
+    ];
+    $scope.currentUser = {
+        name: '',
+        mail: ''
+    };
 
+    $scope.submit = function() {
+        $http.post('/email', $scope.users)
+            .then(function() {
+                console.log('success');
+                $scope.resetForm();
+            }, function() {
+                console.log('error');
+            });
+    };
+
+    $scope.resetForm = function() {
+        $scope.currentUser.name = '';
+        $scope.currentUser.mail = '';
+    };
 }]);
